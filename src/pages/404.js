5848e9e666bced48c53ce402d736af540507768d
Tanner Scadden <tanner@scaddenfamily.com>
2019-05-07 12:30:22 -0600

    added success page, changed submit form

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,8 +3,8 @@ import Layout from '../components/layout'
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <a href="/"><h1>NOT FOUND</h1>
+    <p>Please return to the homepage</p></a>
   </Layout>
 )
 
---
+++ src/pages/success.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const SuccessPage = () => (
+  <Layout>
+    <a href="/"><h1>Success</h1>
+    <p>I will get back to you as soon as possible &#40;:</p></a>
+  </Layout>
+)
+
+export default SuccessPage
