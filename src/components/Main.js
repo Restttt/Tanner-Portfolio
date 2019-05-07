bfcf2c09dce81e40adbf76b571ea077f3be4f3c9
Tanner Scadden <tanner@scaddenfamily.com>
2019-05-07 11:50:39 -0600

    testing fix for netlify contact form

--- src/components/Main.js
+++ src/components/Main.js
@@ -62,14 +62,14 @@ class Main extends React.Component {
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
-          <form method="post" action="#" data-netlify="true" data-netlify-honeypot="bot-field">
+          <form method="post" name="contact" data-netlify="true">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name"  />
             </div>
             <div className="field half">
               <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" name="bot-field" />
+              <input type="text" name="email" id="email"/>
             </div>
             <div className="field">
               <label htmlFor="message">Message</label>
